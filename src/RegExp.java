010e1598771f54a790d3f9bfb94602354f89f39f
Vladyslav Kulichenko <vladyslavkulichenko@TH-vladyslavkulichenko.local>
2020-03-23 19:11:42 +0200

    Task solved

--- src/RegExp.java
+++ src/RegExp.java
@@ -1,15 +1,32 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Regular expressions.
  */
 public class RegExp {
 
-  /**
-   * Get IP address from text.
-   * @param text text to parse
-   * @return String with IP address
-   */
-  public static String getIpAddress(String text) {
-    // TODO fill in code here for extracting IP address using Regular Expression
-    return "";
-  }
+    /**
+     * Get IP address from text.
+     *
+     * @param text text to parse
+     * @return String with IP address
+     */
+    public static String getIpAddress(String text) {
+        String result = "";
+        String string = "\\d+\\.+\\d+\\.+\\d+\\.+\\d+";
+        Pattern pattern = Pattern.compile(string);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+
+            result = matcher.group();
+            double first_group = Double.parseDouble(result.substring(0, 3));
+            if (255 < first_group) {
+                result = "";
+            }
+
+        }
+
+        return result;
+    }
 }
--- src/RegExpTest.java
+++ src/RegExpTest.java
@@ -17,8 +17,9 @@ public class RegExpTest {
         {"http://192.168.1.1/index.html", "192.168.1.1"},
         {"This text contains a valid IP address 254.254.254.254, find it!", "254.254.254.254"},
         {"IP could not have negative numbers like -1.-2.-3.-4", ""},
-        {" Invalid IP: 300.300.300.300!", ""}
-        // TODO add 2 more test data here
+        {" Invalid IP: 300.300.300.300!", ""},
+        {"Прикладом IP-адреси може бути адреса 127.0.0.1 (локальна IP-адреса, змінити її неможливо, і вона на кожній ОС лише одна — localhost).", "127.0.0.1"},
+        {"My Public IP is: 109.108.84.187", "109.108.84.187"}
     };
   }
 
